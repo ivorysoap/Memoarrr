89db7ae153e5cfa40014ec01859b0d9d080ef38f
Ivor Benderavage <ivor.benderavage@gmail.com>
2018-11-02 15:44:09 -0400

    Added stub for Person class

---
+++ Person.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Ivor on 2018-11-02.
+//
+
+#include "Person.h"
+
+
---
+++ Person.h
@@ -0,0 +1,14 @@
+//
+// Created by Ivor on 2018-11-02.
+//
+
+#ifndef MEMOARRR_PERSON_H
+#define MEMOARRR_PERSON_H
+
+
+class Person {
+
+};
+
+
+#endif //MEMOARRR_PERSON_H
