5852af18ae72d3478ea7036efa21a36785b37e9b
Boris Nz <borisnz101@gmail.com>
2018-11-09 15:42:19 -0500

    Added Player header

---
+++ Player.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Player.h
+ * Author: Boris Nz
+ *
+ * Created on 9 novembre 2018, 15:39
+ */
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+
+#endif /* PLAYER_H */
+
