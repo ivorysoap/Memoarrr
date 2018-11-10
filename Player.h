fc17af5cf2095784a9d3c366bb22344064373fcc
Boris Nz <borisnz101@gmail.com>
2018-11-10 00:43:54 -0500

    PLayer .h file

--- Player.h
+++ Player.h
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
  * File:   Player.h
@@ -10,11 +5,31 @@
  *
  * Created on 9 novembre 2018, 15:39
  */
+#include <iostream>
+using namespace std;
 
 #ifndef PLAYER_H
 #define PLAYER_H
 
-
+class Player{
+    
+    string name;
+    enum Side {TOP = 1, BOTTOM, LEFT, RIGHT};
+    int rubies = 0;
+    bool active;
+    
+    
+public:
+    static int player_count;
+    Player(const string& _name);
+    
+    string getName() const {return name;}
+    void setActive(bool);
+    bool isActive() const {return active;}
+    int getNRubies() const {return rubies;}
+    void addReward(const Reward&);
+    void setDisplayMode(bool endOfGame);
+};
 
 #endif /* PLAYER_H */
 
