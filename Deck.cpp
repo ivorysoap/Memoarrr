eb799f5d0c8efd7b086dd6653c2c4d33f897628f
Ivor Benderavage <ivor.benderavage@gmail.com>
2018-11-27 22:35:30 -0500

    Added stub for Deck.cpp

---
+++ Deck.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Deck.h"
+using namespace std;
+
+void Deck::shuffle(){
+
+    //Unimplemented
+
+}
+
+bool Deck::isEmpty(){
+
+    //Unimplemented
+    return false;
+
+}
+
+template <class T>
+T* Deck::getNext(){
+
+    //Unimplemented
+    return nullptr;
+
+}
