4f1c82ea83afc5fa2b957909f04e6dd0daa97ff9
Boris Nz <borisnz101@gmail.com>
2018-11-29 22:13:06 -0500

    Updates on player

--- Player.h
+++ Player.h
@@ -4,13 +4,13 @@
  * Author: Boris Nz
  * Created on 9 novembre 2018, 15:39
  */
+
+#ifndef PLAYER_H
+#define PLAYER_H
 #include <iostream>
 #include "Reward.h"
 
 using namespace std;
-
-#ifndef PLAYER_H
-#define PLAYER_H
 
 class Player{
     
@@ -22,10 +22,10 @@ public:
     enum Side {TOP = 1, BOTTOM, LEFT, RIGHT} enumField;
     static int player_count; //keeps the number of players and thus assign the player side when player object is created
    
-    Player(const string& _name): name(_name){}
+    Player(const string& _name);
     
     string getName() const {return name;}
-    void setActive(bool) {active = 1;}
+    void setActive(bool) {active = true;}
     bool isActive() const {return active;}
     int getNRubies() const {return rubies;}
     void addReward(const Reward&);
