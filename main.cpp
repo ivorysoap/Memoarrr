4ce9dccf67bd8b72003739ba1c70da1ae176d513
Boris Nz <borisnz101@gmail.com>
2018-11-30 01:02:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,8 +11,25 @@
 using namespace std;
 
 int main() {
+    //instance varibles
+    int inputNbPlayers;
+    string inputPlayerName;
     
-    cout << "Welcome to Memoarrr!!!" << endl;
+    cout << "****Welcome to Memoarrr!!!****" << endl << endl;
+    
+    cout << "Please select the number of players" << endl;
+    cin >> inputNbPlayers;
+    
+    while ((inputNbPlayers < 2) && (inputNbPlayers > 4)) {
+        std::cout << "Invalid input! Please enter a digital value between 2 and 4 to select number of players" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(256,'\n');
+        std::cin >> inputNbPlayers;
+    }
+    
+    
+    
+    
     
     
     return 0;
