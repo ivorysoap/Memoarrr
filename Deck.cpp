ab9cd07a51c5a31beba58dcee80c3b64d55bca56
Boris Nz <borisnz101@gmail.com>
2018-11-29 20:49:44 -0500

    implemented getNext()

--- Deck.cpp
+++ Deck.cpp
@@ -1,32 +1,12 @@
-#include <iostream>
+
 #include "Deck.h"
-using namespace std;
-
-Deck::Deck(int numItems=52) {
-
-    for(int i = 0; i < numItems; i++){
-        Deck::deck.push_back(new Card()); //Needs to be fixed to allow for the general case (creating <T> object) rather than just Card!
-    }
-
-}
-
-void Deck::shuffle(){
-
-    //Unimplemented
-
-}
-
-bool Deck::isEmpty(){
-
-    //Unimplemented
-    return false;
-
-}
+#include <iostream>
 
 template <class T>
-T* Deck::getNext(){
-
-    //Unimplemented
-    return nullptr;
-
+T* Deck<T>::getNext() {
+    
+    if(rit!=deck.rend()){
+    return rit++;
+    }else
+    { return nullptr;}
 }
